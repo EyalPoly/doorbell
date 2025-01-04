fe33f8c26d55b11006b136308096a233d6445372
Eyal Politansky <eyalpolitansky@Eyals-MacBook-Pro.local>
2025-01-04 15:43:02 +0200

    Enhanced power consuming with LowPower power down

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,28 +1,37 @@
 #include <RH_ASK.h>
-#include <SPI.h> // Not actually used but RH_ASK.h needs it to compile
+#include <SPI.h> // Required for RH_ASK.h to compile
 #include <LowPower.h>
 
 RH_ASK driver;
 
-#define BUTTON_PIN 5
+#define BUTTON_PIN 3
+
+volatile bool buttonPressed = false;
+
+void buttonInterrupt() {
+  buttonPressed = true;
+}
 
 void setup() {
-  Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonInterrupt, FALLING);
+  
   if (!driver.init()) {
+    Serial.begin(9600);
     Serial.println("init failed");
+    Serial.end();
   }
 }
 
 void loop() {
-  if (digitalRead(BUTTON_PIN)==LOW) {
+  if (buttonPressed) {
+    buttonPressed = false;
+
     const char *msg = "a";
     driver.send((uint8_t *)msg, strlen(msg));
     driver.waitPacketSent();
     delay(200);
   }
-  else {
-    // When button is not pressed, enter idle mode to save power
-    LowPower.idle(SLEEP_1S, ADC_OFF, TIMER2_ON, TIMER1_ON, TIMER0_ON, SPI_OFF, USART0_ON, TWI_OFF);
-  }
+
+70  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
 }
